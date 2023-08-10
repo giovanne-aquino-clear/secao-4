a0b47319438b44c9e8c474cba61957898d19e1dd
giovanne <giovanne.aquino.clear@gmail.com>
2023-08-10 09:01:28 -0300

    cria 2 opções de acesso no server com switch case

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// cria server na web   git
 const http= require('http');
 
 let server = http.createServer((req, res) => {
@@ -6,13 +5,36 @@ let server = http.createServer((req, res) => {
     console.log('URL:', req.url);
     console.log('METHOD:', req.method);
 
-    res.end('OK');
+    switch (req.url) {
+
+        case '/':
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<h1>Olá</h1>');
+
+        break;
+
+        case '/users':
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json')
+        res.end(JSON.stringify({
+            users:[{
+                name: 'Hcode',
+                email: 'contato@hcode.com.br',
+                id: 1
+            }]
+        }));
+
+        break;
+
+    }
 
 });
 
-server.listen(3000, '127.0.0.1', () => {// adiciona endereço e porta do localhost
+server.listen(3000, '127.0.0.1', () => {
 
     console.log("servidor rodando!");
 
 });
-
