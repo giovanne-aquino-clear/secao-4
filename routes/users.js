8ccfac16930f322612a28de3c7d24bddbf5c1614
giovanne <giovanne.aquino.clear@gmail.com>
2023-08-10 11:46:10 -0300

    refatora o codigo e route ID para oter os dados do usuario

--- routes/users.js
+++ routes/users.js
@@ -1,40 +1,48 @@
 let NeDB = require('nedb');
-let db =  new NeDB({
-
-filename: 'users.db', 
-autoload: true 
-
+let db = new NeDB({
+    filename: 'users.db',
+    autoload: true
 });
 
 module.exports = (app) => {
 
     app.get('/users', (req, res) => {
 
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({
-            users:[{
-                name: 'Hcode',
-                email: 'contato@hcode.com.br',
-                id: 1
-            }]
+        db.find({}).sort({ name: 1 }).exec((err, users) => {
+
+            if (err) {
+                console.log(`error: ${err}`);
+                res.status(400).json({
+                    error: err
+                })
+            } else {
+
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json({
+                    users
+                });
+                
+            }
+
         });
 
     });
 
     app.post('/users', (req, res) => {
 
-        db.insert (req.body, (err,user)=>{
+        db.insert(req.body, (err, user) => {
 
-            if(err){
-                console.log(`error: ${err}`);
+            if (err) {
+                console.log(`erro: ${err}`);
                 res.status(400).json({
-                    error:err
+                    error: err
+                });
+            } else {
+                
+                res.status(200).json(user);
 
-            });
-        } else {
-            res.status(200).json( user );
-        }
+            }
 
         });
 
