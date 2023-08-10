415c45b152188e894c29c713cd42ecdf67e22856
giovanne <giovanne.aquino.clear@gmail.com>
2023-08-10 08:20:27 -0300

    teste 1

---
+++ index.js
@@ -0,0 +1,17 @@
+// cria server na web   git
+const http= require('http');
+
+let server = http.createServer((req, res) => {
+
+    console.log('URL:', req.url);
+    console.log('METHOD:', req.method);
+
+    res.end('OK');
+
+});
+
+server.listen(3000, '127.0.0.1', () => {// adiciona endereço e porta do localhost
+
+    console.log("servidor rodando!");
+
+});
